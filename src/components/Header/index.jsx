5275c4fa77de8bebaf9a16174bd54d31748da8e2
Andy Mancuso <andymancuso5@gmail.com>
2023-02-14 11:42:30 -0300

    Creación de landing y organización de archivos

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <>
+      <div>Home</div>
+      <div>Movies</div>
+      <div>TV Shows</div>
+      <div>My Lists</div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,20 @@
+
+import { Header } from '../../components'
+import { Button } from '../../components'
+
+
+import styles from './styles.module.css'
+
+const Home = () => {
+    return (
+    <>
+      <div className={styles.header}><Header></Header></div>
+      <div>Componentes</div>
+      <div>Componentes</div>
+      <div>Componentes</div>
+      <div>Componentes</div>
+    </>
+    )
+  }
+  
+  export default Home
